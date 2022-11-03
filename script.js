9811b25396ad130cc9d34da0900ad06c137659db
Ahmed Mohamed <105979851+AhmedMohamed809@users.noreply.github.com>
2022-11-03 02:37:58 +0000

    complete Level 100

--- script.js
+++ script.js
@@ -5,8 +5,21 @@ function setup() {
 }
 
 function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-}
+  for(let i = 0 ; i < episodeList.length ; i ++ ){
+    const rootElem = document.getElementById("root");
+    const dive = document.createElement("div")
+    const pHeader = document.createElement("p");
+    const img = document.createElement("img");
+    const pSum = document.createElement("p");
+    img.src = episodeList[i].image.medium; // display medium Img
+    pHeader.innerText = `${episodeList[i].name} - S01E0${i + 1} `; // display heder and number of episod
+    pHeader.id="header-episode";
+    dive.id="contenar"
+    pSum.innerHTML = episodeList[i].summary; // display summary
+    rootElem.appendChild(dive)
+    dive.appendChild(pHeader);
+    dive.appendChild(img);
+    dive.appendChild(pSum);
+  }}
 
 window.onload = setup;
