b23ac8797b24dc8ddb0ec24e4708b63f2ad8b5c8
Ahmed Mohamed <105979851+AhmedMohamed809@users.noreply.github.com>
2022-11-04 01:53:47 +0000

    cmplete level 200 & 300

--- script.js
+++ script.js
@@ -1,25 +1,15 @@
 //You can edit ALL of the code here
+import {episodCard,navpart} from "./DisplayCard/episodCard.js"
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
 }
 
 function makePageForEpisodes(episodeList) {
-  for(let i = 0 ; i < episodeList.length ; i ++ ){
-    const rootElem = document.getElementById("root");
-    const dive = document.createElement("div")
-    const pHeader = document.createElement("p");
-    const img = document.createElement("img");
-    const pSum = document.createElement("p");
-    img.src = episodeList[i].image.medium; // display medium Img
-    pHeader.innerText = `${episodeList[i].name} - S01E0${i + 1} `; // display heder and number of episod
-    pHeader.id="header-episode";
-    dive.id="contenar"
-    pSum.innerHTML = episodeList[i].summary; // display summary
-    rootElem.appendChild(dive)
-    dive.appendChild(pHeader);
-    dive.appendChild(img);
-    dive.appendChild(pSum);
-  }}
+  // nav section
+  navpart(episodeList);
+  // display episodCard
+  episodCard(episodeList);
+}
 
 window.onload = setup;
