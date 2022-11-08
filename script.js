c0d8b4bc19f079bfeeab3f1fc3aa4023a0326d90
Ahmed Mohamed <105979851+AhmedMohamed809@users.noreply.github.com>
2022-11-08 23:52:18 +0000

    complete level 350

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //You can edit ALL of the code here
 import { episodCard, navpart } from "./episodCard.js";
 function setup() {
-  const getDataFromApi = fetch("https://api.tvmaze.com/shows/179/episodes")
+  const getDataFromApi = fetch("https://api.tvmaze.com/shows/82/episodes")
     .then((resp) => resp.json())
     .then((data) => makePageForEpisodes(data))
     .catch((error) => console.log(error));
